9973ecf629ee06e8899532b325018c2145d03649
Alex Daniel Meza <alex.daniel.m@trulydigital.tech>
2025-05-18 16:48:14 -0500

    feat: deploy on gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'static',
+    base: 'https://github.com/TrulyDigital/project-documentation',
     integrations: [
         tailwind()
     ]
